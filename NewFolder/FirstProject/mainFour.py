28b7c79d94e1a31e43b06d686dbf3b66595fcf0e
IvanDS-OF <ivan.durans@outlook.com>
2024-06-28 18:56:44 -0600

    Knowing the body parameters

--- NewFolder/FirstProject/mainFour.py
+++ NewFolder/FirstProject/mainFour.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Body
 
 app = FastAPI()
 
@@ -15,4 +15,34 @@ async def get_todo_2(todo_id:int):
     except:
         raise HTTPException(status_code=404, detail="Sobres, no jaló, bai")
 
+##Lets to know how to use the POST method
+#In order to know how to use the body parameters. 
+
+##First we need to import "body" from the FastApi library
+#Define the decorator, methos and path
+@app.post("/todo1")
+async def post_add_item_1(id:int = Body(), description:str = Body(), completed:bool = Body()): 
+    #We must ned to declare our arguments of the function as body parameters, therer could be confusion
+    #with the path parameters
+    try:
+        #Initiali, lets to show the arguments typed
+        return id, description, completed
+    except:
+        raise HTTPException(status_code=404, detail="AYUDA DIOS PLOTS")
+    
+
+##This add the new information to our dictionary and returns all the table updated
+@app.post("/todo")
+async def post_add_item(id:int = Body(), description:str = Body(), completed:bool = Body()): 
+    try:
+        #Add the informatio to my table: Add a new dictionary
+        todo_list.append({
+            "id": id, "description": description, "completed": completed
+        })
+
+
+        return todo_list
+    except:
+        raise HTTPException(status_code=404, detail="AYUDA DIOS PLOTS")
+    
 
